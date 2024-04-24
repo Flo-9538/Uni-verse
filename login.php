4ec37c7b886b747af09a172fb4e74f64995b1560
raphatex <raphaeltexier@aol.com>
2024-04-24 14:31:46 +0200

    documentation du code

--- login.php
+++ login.php
@@ -5,12 +5,16 @@ $user = "root";
 $pass = "root";
 
 $bdd = new PDO("mysql:host=$servername;dbname=universe", $user); 
+// création de l'instance bdd (connexion à la bdd)
 
 if($_SERVER["REQUEST_METHOD"] == "POST"){
     $email = $_POST['email'];
     $password = $_POST['password'];
+    // récupération des champs du form 
+
     if($email != "" && $password != ""){
         $req = $bdd->query("SELECT * FROM users WHERE email = '$email' AND password = '$password'");
+        // requete sql pour la bdd
         $rep = $req->fetch();
         if($rep['id'] != false){
             // c'est ok
--- register.php
+++ register.php
@@ -4,13 +4,16 @@ $user = "root";
 $pass = "root";
 
 $bdd = new PDO("mysql:host=$servername;dbname=universe", $user); 
+// création de l'instance bdd (connexion à la bdd)
 
 if(isset($_POST['ok'])){
     $username = $_POST['username'];
     $email = $_POST['email'];
     $password = $_POST['password'];
+    // récupération des champs du form 
 
     $requete = $bdd->prepare("INSERT INTO users VALUES(0, :username, :email, :password)");
+    // requete sql pour la bdd
     $requete->execute(
         array(
             "username" => $username,
