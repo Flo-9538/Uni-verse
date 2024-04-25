d9c1852c8c85d60fde733bc0f0dbd39f6ffc1ab3
Raphael Texier <raphaeltexier@aol.com>
2024-04-25 18:57:03 +0200

    update sql

--- universe.sql
+++ universe.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : mer. 24 avr. 2024 à 14:35
+-- Généré le : jeu. 25 avr. 2024 à 18:56
 -- Version du serveur : 10.4.32-MariaDB
 -- Version de PHP : 8.2.12
 
@@ -24,32 +24,32 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Structure de la table `users`
+-- Structure de la table `messages`
 --
 
-CREATE TABLE `users` (
+CREATE TABLE `messages` (
   `id` int(255) NOT NULL,
-  `username` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL
+  `user` varchar(255) NOT NULL,
+  `message` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Déchargement des données de la table `users`
+-- Déchargement des données de la table `messages`
 --
 
-INSERT INTO `users` (`id`, `username`, `email`, `password`) VALUES
-(1, 'raphatex', 'raphael.texier', 'Wendylabest45'),
-(2, 'wendy', 'wendy.juju', 'Raphouleplusbo123');
+INSERT INTO `messages` (`id`, `user`, `message`) VALUES
+(1, 'raphatex', 'lorem ipsum hdzq qzdhzuiq hdizq zd qzjdjqzqzd qzd qzpdozqpdo qzd qzd zqd zq dzq opkhph rpoib xcbnxcv xgjyghciushc ijfoiesjdiozqjdoij zdi doqdjozqjd ozjqdoizq djzquidzqpjd qozidiozqj dzdpo zqopd zodzq'),
+(2, 'flo', 'les ordinateurs kantiks >>>>>>> arduino >>>> les maths'),
+(4, 'pipi', 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa');
 
 --
 -- Index pour les tables déchargées
 --
 
 --
--- Index pour la table `users`
+-- Index pour la table `messages`
 --
-ALTER TABLE `users`
+ALTER TABLE `messages`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -57,10 +57,10 @@ ALTER TABLE `users`
 --
 
 --
--- AUTO_INCREMENT pour la table `users`
+-- AUTO_INCREMENT pour la table `messages`
 --
-ALTER TABLE `users`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+ALTER TABLE `messages`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
