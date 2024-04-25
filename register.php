f87a262e5cbfdb9a63210b1fb4f41c879c815a18
Raphael Texier <raphaeltexier@aol.com>
2024-04-25 15:21:23 +0200

    session

--- register.php
+++ register.php
@@ -9,7 +9,7 @@ $bdd = new PDO("mysql:host=$servername;dbname=universe", $user);
 if(isset($_POST['ok'])){
     $username = $_POST['username'];
     $email = $_POST['email'];
-    $password = $_POST['password'];
+    $password = sha1($_POST['password']);
     // récupération des champs du form 
 
     $requete = $bdd->prepare("INSERT INTO users VALUES(0, :username, :email, :password)");
