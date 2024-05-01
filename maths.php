3ef4f7ae50de58b211a52e62ff71b3751f57884e
raphatex <raphaeltexier@aol.com>
2024-05-01 19:18:23 +0200

    php

--- maths.php
+++ maths.php
@@ -1,7 +1,8 @@
 <?php
 include 'bdd.php';
 
-$get_messages = $bdd->query("SELECT `user`,`message`,`date` FROM `messages` WHERE `category`='maths' ORDER BY `id` DESC");
+$category = 'maths';
+$get_messages = $bdd->query("SELECT `user`,`message`,`date` FROM `messages` WHERE `category`='$category' ORDER BY `id` DESC");
 $page = "maths";
 
 include 'main.php';
