605f97e9b1b79631fb74a1a9ad8859c53387d788
Flo-9538 <f.mary-lachko@orange.fr>
2024-05-03 14:47:06 +0200

    modif bdd

--- universe.sql
+++ universe.sql
@@ -24,43 +24,6 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Structure de la table `messages`
---
-
-CREATE TABLE `messages` (
-  `id` int(11) NOT NULL,
-  `user` varchar(255) NOT NULL,
-  `message` text NOT NULL,
-  `category` varchar(255) NOT NULL,
-  `date` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Déchargement des données de la table `messages`
---
-
-INSERT INTO `messages` (`id`, `user`, `message`, `category`, `date`) VALUES
-(2, 'test', 'vous avez fait le dm ?', 'maths', '0000-00-00 00:00:00'),
-(4, 'test', 'bonsoir', 'general', '2024-04-29 13:43:05'),
-(5, 'test', 'yo', 'general', '2024-04-30 10:06:20'),
-(6, 'test', 'm to the b', 'general', '2024-04-30 10:06:25'),
-(7, 'test', 'pump it up', 'general', '2024-04-30 10:06:32'),
-(8, 'test', 'c moi', 'general', '2024-04-30 10:08:14'),
-(9, 'test', 'zqoidio', 'general', '2024-04-30 12:59:39'),
-(10, 'test', 'ksef\r\nzdzqd\r\nzqdozqd$zqd$\r\nzmqd$\r\nzq', 'general', '2024-04-30 12:59:43'),
-(11, 'test', 'jnde', 'general', '2024-04-30 12:59:47'),
-(12, 'test', 'qhdkzqhuod', 'general', '2024-04-30 12:59:50'),
-(13, 'test', 'jzduizahdiozahdpi', 'general', '2024-04-30 12:59:55'),
-(14, 'test', 'bonsoir,\r\ncomment allez vous ?\r\nbien cordialement\r\nRaphatex', 'general', '2024-04-30 13:11:32'),
-(15, 'test', 'cc', 'general', '2024-04-30 13:21:00'),
-(16, 'florent', "hello c'est moi\r\nFlorent", 'general', '2024-04-30 13:30:24'),
-(17, 'raphatex', 'coucou florent', 'general', '2024-04-30 13:31:08'),
-(18, 'raphatex', 'non pas encore\r\net florent ?', 'maths', '2024-04-30 13:31:33'),
-(19, 'florent', 'toujours pas les gars on devrait se dépêcher', 'maths', '2024-04-30 13:32:09');
-
--- --------------------------------------------------------
-
---
 -- Structure de la table `users`
 --
 
@@ -74,15 +37,25 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Déchargement des données de la table `users`
+-- Structure de la table `messages`
 --
 
-INSERT INTO `users` (`id`, `username`, `email`, `password`, `active`, `id_coded`) VALUES
-(24, 'raphatex', 'raphael.texier', '40bd001563085fc35165329ea1ff5c5ecbdbbeef', 1, '4d134bc072212ace2df385dae143139da74ec0ef');
+CREATE TABLE `messages` (
+  `id` int(11) NOT NULL,
+  `user-id` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `message` text NOT NULL,
+  `category` varchar(255) NOT NULL,
+  `date` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+
+----------------------------------------------------------
 --
--- Index pour les tables déchargées
+-- Index pour la table `users`
 --
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Index pour la table `messages`
@@ -90,11 +63,26 @@ INSERT INTO `users` (`id`, `username`, `email`, `password`, `active`, `id_coded`
 ALTER TABLE `messages`
   ADD PRIMARY KEY (`id`);
 
+ALTER TABLE `messages`
+  ADD FOREIGN KEY (`user-id`) REFERENCES `users`(`id`);
+
+
+-- --------------------------------------------------------
 --
--- Index pour la table `users`
+-- Déchargement des données de la table `users`
 --
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
+
+INSERT INTO `users` (`id`, `username`, `email`, `password`, `active`, `id_coded`) VALUES
+(1, 'raphatex', 'raphael.texier', '40bd001563085fc35165329ea1ff5c5ecbdbbeef', 1, '4d134bc072212ace2df385dae143139da74ec0ef');
+--
+-- Déchargement des données de la table `messages`
+--
+
+INSERT INTO `messages` (`id`,`user-id`, `username`, `message`, `category`, `date`) VALUES
+(1, 1, 'raphatex', "Bonjour, bienvenue à tous sur Uni'verse, un lieu d'échange créé par des étudiants, pour les étudiants, j'espère que vous passerez un bon moment ici!", 'general', '2024-05-03 18:00:00');
+
+
+--------------------------------------------------------
 
 --
 -- AUTO_INCREMENT pour les tables déchargées
